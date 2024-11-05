5d0921ea7d7bef99e5be5f662c9bf0acd21e5f93
Santhosh-25 <122344859+Santhosh-25@users.noreply.github.com>
2024-11-05 19:46:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!! From the deployed react application !!</h1>
+        <h1>Updating the Front page</h1>
       </header>
     </div>
   );
