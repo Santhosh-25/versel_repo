78ab210bc75c94fa6098747338f80a1479e920f8
Santhosh-25 <122344859+Santhosh-25@users.noreply.github.com>
2024-11-05 18:41:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!! From the deployed react application</h1>
+        <h1>Hello World!! From the deployed react application !!</h1>
       </header>
     </div>
   );
